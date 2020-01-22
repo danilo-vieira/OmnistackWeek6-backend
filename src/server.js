715b5b1f53374fc96c0e6a6fb89ec45755cc6b36
Danilo Vieira <danilo_strvieira@hotmail.com>
2020-01-22 04:18:36 -0300

    Adicionado variáveis de embiente

--- src/server.js
+++ src/server.js
@@ -41,4 +41,4 @@ app.use("/files", express.static(path.resolve(__dirname, '..', 'tmp')))
 
 app.use(require('./routes'));
 
-server.listen(3333); // Porta a ser usada pelo Node.js
+server.listen(process.env.PORT || 3333); // Porta a ser usada pelo Node.js
